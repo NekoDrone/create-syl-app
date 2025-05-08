916ac05864c5d6322e250a29d7ecf3fd9b231789
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-05-09 01:02:40 +0800

    feat: test function to scaffold

---
+++ index.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import logSomething from "@/createProject";
+
+logSomething("✨  Welcome to create-syl-app ✨ ")
---
+++ src/createProject.ts
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+
+export default function logSomething(message: string) {
+  console.log(chalk.cyan(message))
+}
