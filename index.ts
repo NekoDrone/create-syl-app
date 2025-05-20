4978f518167e4feeaa7cec42767aa8388dd860a6
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-05-20 16:13:22 +0800

    feat: log messages

--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
 #!/usr/bin/env node
+import * as inquirer from "@inquirer/prompts";
+import { PrintColour, printWithColour } from "@/utils/logging";
+import * as path from "node:path";
 
-import logSomething from "@/createProject";
+async function main() {
+  printWithColour(PrintColour.MAGENTA, " ✨ 🐰 Welcome to create-syl-app 🐰✨ ", {
+    bold: true,
+  });
+  const projectName = await inquirer.input({
+    message:
+      "What is the name of your project? (enter '.' to use the current directory)",
+    validate: (input) => input.trim() !== "" || "Project name is required",
+  });
+  printWithColour(
+    PrintColour.WHITE,
+    `Bootstrapping new project${projectName === "." ? ` in current folder: ${path.basename(process.cwd())}` : `: ${projectName}`}`,
+  );
+}
 
-logSomething("✨  Welcome to create-syl-app ✨ ")
+main().then(() => {
+  printWithColour(PrintColour.GREEN, " ✨  Finished! Have fun building! 💜️ ")
+});
